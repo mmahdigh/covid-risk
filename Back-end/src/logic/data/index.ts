570ce12d40f43960cf9144f0624a15373b3a8e8e
Mahdi <mahdighajary@gmail.com>
2020-12-26 18:29:19 +0330

    update region data

--- Back-end/src/logic/data/index.ts
+++ Back-end/src/logic/data/index.ts
@@ -1,5 +1,5 @@
 import csv from "csv-parser";
-import fs from "fs";
+import * as fs from "fs";
 import { CovidData, DataPoint, RegionData } from "../data";
 import { RegionCovidData } from "../regionData";
 
@@ -38,23 +38,29 @@ const calcPopulationInMillion = (allCases: number, incideneRate: number) => {
 
 export const updateData = async (time: string, weekAgo: string) => {
 
-  const results1 = await readData("./src/logic/data/covid-JHU-data/" + time + ".csv")
-  const results2 = await readData("./src/logic/data/covid-JHU-data/" + weekAgo + ".csv")
+  const results1 = await readData("./covid-jhu-data/" + time + ".csv")
+  const results2 = await readData("./covid-jhu-data/" + weekAgo + ".csv")
 
   let final : {[province: string]: DataPoint} = {};
 
   results1.forEach((item, index) => {
-    const itemLastWeek = results2[index]
+    const itemLastWeek = results2.find((el) => el.Combined_Key === item.Combined_Key)
+    if (!item || !itemLastWeek || !itemLastWeek.Deaths || item.Deaths === '0' || item.Confirmed === '0') return;
+    
     const averageDeathLastWeek = (!item.Deaths || Number(item.Deaths) === 0) ? 0 :(Number(item.Deaths) -
      Number(itemLastWeek.Deaths)) / 7
 
+     if (item.Country_Region === "Iran") {
+       console.log(item)
+       console.log(itemLastWeek)
+       console.log(averageDeathLastWeek);
+     }
     const population = calcPopulationInMillion(Number(item.Confirmed),
     Number(item.Incident_Rate));
     
     const keyName = item.Admin2 ? `${item.Country_Region} - ${item.Province_State} - ${item.Admin2}` 
     : item.Province_State ? `${item.Country_Region} - ${item.Province_State}` : item.Country_Region
 
-    if (item.Deaths === '0' || item.Confirmed === '0') return;
 
     if (keyName.includes('unknown') || keyName.includes('Unassigned')) return;
 
@@ -75,16 +81,11 @@ export const updateData = async (time: string, weekAgo: string) => {
     else newObj = regionData
     newData = {...newData, [key]: newObj};
   })
-  let newFinal : CovidData = {}
 
-  Object.keys(final).forEach((key) => {
-    if (newData[key]) return 
-    const newObj : RegionData = {medianAge: 35, dataPoints: [final[key]]}
-    newFinal = {...newFinal, [key]: newObj}
-  })
-
-  console.log(JSON.stringify({...newData, ...newFinal}));
-
+  fs.writeFile('result.txt', JSON.stringify({...newData}), function (err) {
+    if (err) return console.log(err);
+    console.log('Hello World > helloworld.txt');
+  });
 };
 
-updateData("12-20-2020", "12-13-2020");
+updateData("11-29-2020", "11-22-2020");
