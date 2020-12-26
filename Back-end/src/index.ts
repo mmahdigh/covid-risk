76bd1acf688b92b9086e9373ec79d6ad9d07edd9
Mahdi <mahdighajary@gmail.com>
2020-12-26 15:47:36 +0330

    fix backend start script

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import { router } from './src/root/index';
+import { router } from './root/index';
 
 const app = express()
 const port = 7070
